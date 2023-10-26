9b77b4a906deaa4e9e9375e35426f8565fd4f22c
Higor Nóbrega <higorst.nobrega@gmail.com>
2023-10-26 10:11:37 -0300

    update

--- prisma/migrations/20231026131045_updatebd/migration.sql
+++ prisma/migrations/20231026131045_updatebd/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "ativo" BOOLEAN NOT NULL DEFAULT true,
     "email" TEXT NOT NULL,
@@ -8,19 +8,23 @@ CREATE TABLE "User" (
     "matricula" TEXT,
     "avatarUrl" TEXT,
     "autorizacao_pesquisa" BOOLEAN NOT NULL DEFAULT false,
-    "tipo" TEXT NOT NULL DEFAULT 'Aluno'
+    "tipo" TEXT NOT NULL DEFAULT 'Aluno',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Supermercado" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "pesquisaIdS" TEXT
+    "pesquisaIdS" TEXT,
+
+    CONSTRAINT "Supermercado_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PesquisaGeral" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "mes_ano" TEXT NOT NULL,
     "ativo" BOOLEAN NOT NULL DEFAULT true,
     "nome_supermercado" TEXT NOT NULL,
@@ -35,12 +39,14 @@ CREATE TABLE "PesquisaGeral" (
     "acucar" INTEGER,
     "banana" INTEGER,
     "oleoSoja" INTEGER,
-    "manteiga" INTEGER
+    "manteiga" INTEGER,
+
+    CONSTRAINT "PesquisaGeral_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PesquisaCompleta" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "ativo" BOOLEAN NOT NULL DEFAULT true,
     "mes_ano" TEXT NOT NULL,
     "finalizado" BOOLEAN NOT NULL DEFAULT false,
@@ -86,24 +92,27 @@ CREATE TABLE "PesquisaCompleta" (
     "manteiga" INTEGER,
     "userId" TEXT,
     "PesquisaGeral_id" TEXT,
-    CONSTRAINT "PesquisaCompleta_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "PesquisaCompleta_PesquisaGeral_id_fkey" FOREIGN KEY ("PesquisaGeral_id") REFERENCES "PesquisaGeral" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "PesquisaCompleta_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PesProd" (
-    "id" TEXT NOT NULL PRIMARY KEY
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "PesProd_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Produto" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "peso_qtd" TEXT NOT NULL,
     "tipo_qtd" TEXT NOT NULL,
     "preco" INTEGER NOT NULL,
     "PesProdPr" TEXT,
-    CONSTRAINT "Produto_PesProdPr_fkey" FOREIGN KEY ("PesProdPr") REFERENCES "PesProd" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Produto_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -111,3 +120,12 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
+
+-- AddForeignKey
+ALTER TABLE "PesquisaCompleta" ADD CONSTRAINT "PesquisaCompleta_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PesquisaCompleta" ADD CONSTRAINT "PesquisaCompleta_PesquisaGeral_id_fkey" FOREIGN KEY ("PesquisaGeral_id") REFERENCES "PesquisaGeral"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Produto" ADD CONSTRAINT "Produto_PesProdPr_fkey" FOREIGN KEY ("PesProdPr") REFERENCES "PesProd"("id") ON DELETE SET NULL ON UPDATE CASCADE;
